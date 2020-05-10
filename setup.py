58c04582e97ca53bf8712ccb64cf6fcf9fbaaf4d
Johnny Foulds <hfoulds@gmail.com>
2020-05-10 20:08:19 +0200

    Blah, bored of this now

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 	install_requires=['absl-py==0.7.0', 'astor==0.7.1', 'bleach==1.5.0', 'click==6.7',
 	                  'cycler==0.10.0', 'dateparser@git+https://github.com/scrapinghub/dateparser.git@a01a4d2071a8f1d4b368543e5e09cde5eb880799', 'Flask==1.0.2', 'gast==0.2.2', 'grpcio==1.14.0', 'h5py==2.8.0',
 	                  'html5lib==0.9999999', 'itsdangerous==0.24', 'Jinja2==2.10', 'Keras==2.2.1',
-	                  'kiwisolver==1.0.1', 'Markdown==2.6.11', 'MarkupSafe==1.0', 'matplotlib==2.0.3', 'nose==1.3.7', 'numpy==1.17.0',
+	                  'kiwisolver==1.0.1', 'Markdown==2.6.11', 'MarkupSafe==1.0', 'matplotlib==2.2.5', 'nose==1.3.7', 'numpy==1.17.0',
 	                  'pandas==0.24.2', 'protobuf==3.6.1', 'pyparsing==2.2.0', 'python-dateutil==2.7.3', 'pytz==2018.5', 'python-Levenshtein',
 	                  'PyYAML==3.13', 'scikit-learn==0.22.0', 'scipy==1.3.1', 'six==1.11.0',
 	                  'sklearn==0.0', 'tensorflow==1.15', 'termcolor==1.1.0',
