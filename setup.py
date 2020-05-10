aad6e22de59ca18ad4c84040f54ec72038874296
Johnny Foulds <hfoulds@gmail.com>
2020-05-10 20:20:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
 	                  'kiwisolver==1.0.1', 'Markdown==2.6.11', 'MarkupSafe==1.0', 'matplotlib==3.0.0', 'nose==1.3.7', 'numpy==1.17.0',
 	                  'pandas==0.24.2', 'protobuf==3.6.1', 'pyparsing==2.2.0', 'python-dateutil==2.7.3', 'pytz==2018.5', 'python-Levenshtein',
 	                  'PyYAML==5.3', 'scikit-learn==0.22.0', 'scipy==1.3.1', 'six==1.11.0',
-	                  'sklearn==0.22.1', 'tensorflow==1.15', 'termcolor==1.1.0',
+	                  'sklearn', 'tensorflow==1.15', 'termcolor==1.1.0',
 	                  'Werkzeug==0.14.1'],
 )
